07b3676b045bcd69b87fb45ea070293cd0b98b38
BoggyDan <danieladewale2006@gmail.com>
2022-11-28 15:58:16 +0100

    Cleaning up

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,53 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
-int main(void)
+int check_cycle(listint_t *list)
 {
-	listint_t *head;
-	listint_t *current;
-	listint_t *temp;
-	int i;
+	listint_t *p2;
+	listint_t *prev;
 
-	head = NULL;
-	add_nodeint(&head, 0);
-	add_nodeint(&head, 1);
-	add_nodeint(&head, 2);
-	add_nodeint(&head, 3);
-	add_nodeint(&head, 4);
-	add_nodeint(&head, 98);
-	add_nodeint(&head, 402);
-	add_nodeint(&head, 1024);
-	print_listint(head);
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
+	{
+		list = list->next;
+		p2 = p2->next->next;
 
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
 
-	current = head;
-	for (i = 0; i < 4; i++)
-		current = current->next;
-	temp = current->next;
-	current->next = head;
-
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-		current = current->next;
-	current->next = temp;
-
-	free_listint(head);
+				list = list->next;
+			}
+			return (1);
+		}
+	}
 
 	return (0);
 }
