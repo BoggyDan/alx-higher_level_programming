493e3769d9ebac98436f2635a84ab7bc48fc2ad8
BoggyDan <danieladewale2006@gmail.com>
2022-11-29 13:22:27 +0100

    Working with if and else in python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 99):
+    if number % 10 > number / 10:
+        if number != 89:
+            print(f"{number:02}", end=', ')
+        else:
+            print(f"{number:02}")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for alphabet in str:
+        letter_rep = ord(alphabet)
+        if letter_rep >= 97 and letter_rep <= 122:
+            letter_rep = letter_rep - 32
+        letter = chr(letter_rep)
+        print(letter, end='')
+    print('')
