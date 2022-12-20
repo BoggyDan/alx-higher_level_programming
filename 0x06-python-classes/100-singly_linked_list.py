2a9d21643616ba14148bfddfaee058dcbba9308e
BoggyDan <danieladewale2006@gmail.com>
2022-12-20 15:19:22 +0100

    Fixing files

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,32 +6,39 @@ and initializes it with a size
 
 
 class Node:
+    """Class that creates the node of a singly linked list"""
     def __init__(self, data, next_node=None):
+        """Initialize method that stores a data and the a node"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Method that returns data"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Method that sets the data"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Method that sets a node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """Method that sets the node"""
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Class that creates a singly linked list"""
     def __str__(self):
         rtn = ""
         ptr = self.__head
@@ -45,9 +52,11 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """Initialize method to make the head node none"""
         self.__head = None
 
     def sorted_insert(self, value):
+        """Method to set the pointer variable to head"""
         ptr = self.__head
 
         while ptr is not None:
