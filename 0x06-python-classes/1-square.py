55d33d2e8c36cc231cd0587e98b87d03dc13e9c3
BoggyDan <danieladewale2006@gmail.com>
2022-12-20 14:23:09 +0100

    Fixing errors

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square module
+This module contains a class that defines a square
+and initializes it with a size
+"""
+
 
 class Square:
     """Class Square that defines a square object
