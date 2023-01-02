cc78ff13b291e9809d485b62b5fb02b0a9956734
BoggyDan <danieladewale2006@gmail.com>
2023-01-02 14:44:17 +0100

    101-nqueens.py file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1 or len(sys.argv) > 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
