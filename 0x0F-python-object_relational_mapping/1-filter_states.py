54255a70ddd00673c895679b883e32310c8b9d37
BoggyDan <danieladewale2006@gmail.com>
2023-03-18 17:14:33 +0100

    Still working on it

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * states WHERE name REGEXP '^N.*$';")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N.*$';")
     states = cur.fetchall()
 
     for state in states:
